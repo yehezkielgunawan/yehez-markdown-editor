471c34bfa54055620615420805f9870fae485562
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-12-01 20:08:34 +0700

    build(yarn.lock): :package: change to yarn and change the example input

--- src/constants/dummyInput.ts
+++ src/constants/dummyInput.ts
@@ -10,6 +10,22 @@ This project was scaffolded using [yehez-vitechakra-starter](https://github.com/
 
 Actually, I made it for myself. But of course I'm open to any feedbacks. You can fork or clone this repo and make a PR here if you have some idea.
 
+# Table of Content
+
+[What I'm using here?](https://github.com/yehezkielgunawan/[Repo-name]#what-im-using-here)
+
+[What's included here?](https://github.com/yehezkielgunawan/[Repo-name]#-whats-included-here)
+
+[How to use it?](https://github.com/yehezkielgunawan/[Repo-name]#how-to-use-it)
+
+[How to run it locally?](https://github.com/yehezkielgunawan/[Repo-name]#how-to-run-it-locally)
+
+[References](https://github.com/yehezkielgunawan/[Repo-name]#references)
+
+[Additional Notes](https://github.com/yehezkielgunawan/[Repo-name]#additional-notes)
+
+---
+
 ## What I'm use here?
 
 - [Vite](https://vitejs.dev/guide/#scaffolding-your-first-vite-project) as build tool.
@@ -27,6 +43,10 @@ Actually, I made it for myself. But of course I'm open to any feedbacks. You can
 - You can input your markdown text at the textarea input, and the preview will be displayed live.
 - Here's [Markdown Cheatsheet](https://www.markdownguide.org/cheat-sheet) to help you write the markdown text.
 
+## How to run it locally?
+
+- Git clone (the link)
+
 ## References
 
 - [Vite Docs](https://vitejs.dev/) - Learn about Vite and its features
@@ -35,10 +55,7 @@ Actually, I made it for myself. But of course I'm open to any feedbacks. You can
 - [React Markdown Preview](https://github.com/uiwjs/react-markdown-preview) - For the markdown previewer component.
 - [Freepik Image](https://image.flaticon.com/icons/png/512/1031/1031982.png) - For the image asset.
 
-## Future Improvement
-
-- [x] Reset the textarea button
-- [x] Copy the clipboard button
+## Additional Notes
 
 
 `;
